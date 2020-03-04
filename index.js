4a900d87a2c3421883125ba0058bea5d22c46be1
lgarcia82 <garcialx482@gmail.com>
2020-03-04 14:47:00 -0600

    refactor: finalizing readme, structuring folder layout between backend and frontend components

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+
+// Middleware
+app.use(bodyParser.json())
+
+
+
+// Server
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
