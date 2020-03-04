157b4c65fbeb26c9df592c7592b38c856f013644
lgarcia82 <garcialx482@gmail.com>
2020-03-04 16:23:36 -0600

    feature: footer component added

--- client/src/react/App.js
+++ client/src/react/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import Header from './components/landing-page/Header'
 import BarberList from './components/landing-page/BarberList'
+import Footer from './components/landing-page/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Header />
         <BarberList />
+        <Footer />
       </Router>
     
     </div>
---
+++ client/src/react/components/landing-page/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return(
+    <div className='Footer-Container'>
+      <div className='Footer'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Footer
