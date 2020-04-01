474d81fd1585fbc6b83d8b2d6c0d2ade8f9afd56
Jake <jgarcia267@islander.tamucc.edu>
2020-03-31 19:34:02 -0500

    updating database uri

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const uri = `mongodb+srv://jgarcia:DJfpHi1eg27TUOAg@cluster0-wlsen.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://jgarcia:DJfpHi1eg27TUOAg@cluster0-wlsen.mongodb.net/project-barbershop?retryWrites=true&w=majority`;
 
 const connection = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').set('debug', true);
 
 module.exports = UserSchema = new mongoose.Schema(
   {
